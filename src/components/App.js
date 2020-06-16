6b2f4b0617d6b6261f59bf1f4bc21708a3863934
RamySaleh <ramy.saleh.me@gmail.com>
2020-06-16 13:26:53 +0100

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,12 @@ import "../App.css";
 
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import { Cart } from "./Cart";
 import { Products } from "./Products";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  root: {
-    width: 500,
-  },
-});
-
 function App() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
   const [cart, setCart] = React.useState([]);
 
   const handleAddToCart = (product) => {
@@ -43,13 +33,6 @@ function App() {
 
           <hr />
 
-          {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
           <Switch>
             <Route path="/products">
               <Products onAddToCart={handleAddToCart} />
